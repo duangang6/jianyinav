4bb6863eacf1180e3a33fe04e50cf8ede36bffae
duangang6 <1849751871@qq.com>
2022-07-24 19:22:32 +0800

    Initial commit

---
+++ jianyinav/src/main/java/com/example/jianyinav/CollectFragment.java
@@ -0,0 +1,28 @@
+package com.example.jianyinav;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class CollectFragment extends androidx.fragment.app.Fragment {
+    private int content;
+
+    private ImageView tv_content;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.collect_view, container, false);
+        return view;
+    }
+
+    public void setContent(int content) {
+        this.content = content;
+        tv_content.setImageResource(content);
+    }
+}
